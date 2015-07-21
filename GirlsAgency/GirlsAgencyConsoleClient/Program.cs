bc89e336424808c404cd2505d7c50b0089584d2f
ibakyrdjiev <ibakyrdjiev@gmail.com>
2015-07-21 15:28:50 +0300

    fixed references

--- GirlsAgency/GirlsAgencyConsoleClient/Program.cs
+++ GirlsAgency/GirlsAgencyConsoleClient/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO.Compression;
+using GirlsAgency.Data;
+using GirlsAgency.Model;
+using GirlsAgency.Model.Enums;
+using GirlsAgency.Repository;
+using GirlsAgencyOracle.Data;
 
 namespace GirlsAgencyConsoleClient
 {
@@ -24,20 +30,74 @@ namespace GirlsAgencyConsoleClient
             //    }
             //};
 
-            const string zipPath = @"C:\Users\v.indzhev\Desktop\Bunker.zip";
-            const string extractPath = @"C:\Users\v.indzhev\Desktop\Bunker\";
-            //ZipFile.ExtractToDirectory(zipPath, extractPath);
+            //const string zipPath = @"C:\Users\v.indzhev\Desktop\Bunker.zip";
+            //const string extractPath = @"C:\Users\v.indzhev\Desktop\Bunker\";
+            ////ZipFile.ExtractToDirectory(zipPath, extractPath);
 
 
-            var test = GirlsAgency.Data.FileManipulations.Excel.ExportData(@"C:\Users\v.indzhev\Desktop\Bunker\", @"Importer.xlsx");
-            foreach (var t in test)
-            {
-                Console.WriteLine(t);
-            }
+            //var test = GirlsAgency.Data.FileManipulations.Excel.ExportData(@"C:\Users\v.indzhev\Desktop\Bunker\", @"Importer.xlsx");
+            //foreach (var t in test)
+            //{
+            //    Console.WriteLine(t);
+            //}
 
             //context.Girls.Add(girl);
             //context.SaveChanges();
             //Console.WriteLine(girlsCount);
+
+
+
+
+
+
+            var oracle = new OracleContext();
+            var sqlServer = new GirlsAgencyContext();
+
+            var controller = new RepositoryController(oracle, sqlServer);
+
+            var girl = new Girl()
+            {
+                FirstName = "Minka",
+                LastName = "Svirchoka",
+                Age = 18,
+                BreastSizeId = (int)BreastSizeEnum.Normal,
+                HairColorId = (int)HairColorEnum.Blonde,
+                PricePerHour = 80.0M,
+                Features = new HashSet<Feature>
+                {
+                    new Feature {Name = GirlsFeaturesEnum.Felatio.ToString()},
+                    new Feature {Name = GirlsFeaturesEnum.DoggyStyle.ToString()}
+                }
+            };
+
+
+
+            var girl2 = new Girl()
+            {
+                FirstName = "kur",
+                LastName = "Svirchoka",
+                Age = 18,
+                BreastSizeId = (int)BreastSizeEnum.Normal,
+                HairColorId = (int)HairColorEnum.Blonde,
+                PricePerHour = 80.0M,
+                Features = new HashSet<Feature>
+                {
+                    new Feature {Name = GirlsFeaturesEnum.Felatio.ToString()},
+                    new Feature {Name = GirlsFeaturesEnum.DoggyStyle.ToString()}
+                }
+            };
+
+
+            ICollection<Girl> girls = new List<Girl>();
+            girls.Add(girl);
+            girls.Add(girl2);
+
+            controller.ImportGirlsToDatabase(girls);
+
+
+
+
+
         }
     }
 }
