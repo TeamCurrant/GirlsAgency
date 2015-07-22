96a85b611452f820fcdfa0bc2c27d7b0c6a6d755
ibakyrdjiev <ibakyrdjiev@gmail.com>
2015-07-22 16:59:20 +0300

    Interface fix

--- GirlsAgency/GirlsAgencyOracle.Data/Migrations/Configuration.cs
+++ GirlsAgency/GirlsAgencyOracle.Data/Migrations/Configuration.cs
@@ -1,18 +1,15 @@
+using System.Data.Entity.Migrations;
+
 namespace GirlsAgencyOracle.Data.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<GirlsAgencyOracle.Data.OracleContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<OracleContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(GirlsAgencyOracle.Data.OracleContext context)
+        protected override void Seed(OracleContext context)
         {
             //  This method will be called after migrating to the latest version.
 
--- GirlsAgency/GirlsAgencyOracle.Data/OracleContext.cs
+++ GirlsAgency/GirlsAgencyOracle.Data/OracleContext.cs
@@ -1,25 +1,15 @@
-using GirlsAgency.Model;
-using GirlsAgency.Repository.Contracts;
-using GirlsAgencyOracle.Data.Migrations;
-
 namespace GirlsAgencyOracle.Data
 {
-    using System;
     using System.Data.Entity;
-    using System.Linq;
+    using GirlsAgency.Model;
+    using GirlsAgency.Repository.Contracts;
+
 
     public class OracleContext : DbContext, IContext
     {
-        // Your context has been configured to use a 'OracleContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'GirlsAgencyOracle.Data.OracleContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'OracleContext' 
-        // connection string in the application configuration file.
         public OracleContext()
             : base("OracleDbContext")
         {
-           // Database.SetInitializer(new MigrateDatabaseToLatestVersion<OracleContext, Configuration>());
             Database.SetInitializer(new DropCreateDatabaseAlways<OracleContext>());
         }
 
@@ -29,36 +19,13 @@ namespace GirlsAgencyOracle.Data
             base.OnModelCreating(modelBuilder);
         }
 
+        public virtual IDbSet<Girl> Girls { get; set; }
 
-        public virtual DbSet<Girl> Girls { get; set; }
-
-        //public virtual DbSet<BreastSize> BreastSizes { get; set; }
-
-        //public virtual DbSet<Country> Countries { get; set; }
-
-        //public virtual DbSet<Feature> Features { get; set; }
-
-        //public virtual DbSet<HairColor> HairColors { get; set; }
-
-        public virtual DbSet<Customer> Customers { get; set; }
-        public void SaveChanges()
+        public virtual IDbSet<Customer> Customers { get; set; }
+      
+        public IDbSet<TEntity> Set<TEntity>() where TEntity : class
         {
-            base.SaveChanges();
+            return base.Set<TEntity>();
         }
-
-        //public virtual DbSet<City> Cities { get; set; }
-
-        
-
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
     }
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
