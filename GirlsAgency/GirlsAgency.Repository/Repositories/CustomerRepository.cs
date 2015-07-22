0ec95eb239c587474cae3e9bcfd292867cf761df
ibakyrdjiev <ibakyrdjiev@gmail.com>
2015-07-22 16:58:41 +0300

    Repo added/modified with crud, search with function by pattern, JSON.. All based on interfaces.

---
+++ GirlsAgency/GirlsAgency.Repository/Repositories/CustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace GirlsAgency.Repository.Repositories
+{
+
+    using GirlsAgency.Model;
+    using GirlsAgency.Repository.Contracts;
+    
+    /// <summary>
+    /// Custom Customer Repository
+    /// May contains custom methods
+    /// </summary>
+
+    public class CustomerRepository : GenericRepository<Customer>
+    {
+        public CustomerRepository(IContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ GirlsAgency/GirlsAgency.Repository/Repositories/GirlsRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace GirlsAgency.Repository.Repositories
+{
+    using GirlsAgency.Model;
+    using GirlsAgency.Repository.Contracts;
+
+    /// <summary>
+    /// Custom repo if someone needed
+    /// May contains custom CRUD operations
+    /// </summary>
+    
+    public class GirlsRepository : GenericRepository<Girl>
+    {
+        public GirlsRepository(IContext context) : base(context)
+        {
+        }
+    }
+}
