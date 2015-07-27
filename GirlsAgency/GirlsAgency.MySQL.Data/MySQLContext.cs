45443d9924179acaf7d9466913b66e3b9ac31fa9
enevlogiev <edward.evlogiev@gmail.com>
2015-07-27 12:44:27 +0300

    pushni be mamka ti

--- GirlsAgency/GirlsAgency.MySQL.Data/MySQLContext.cs
+++ GirlsAgency/GirlsAgency.MySQL.Data/MySQLContext.cs
@@ -4,8 +4,11 @@ namespace GirlsAgency.MySQL.Data
     using System.Data.Entity;
     using System.Linq;
     using GirlsAgency.Model;
+    using GirlsAgency.Repository.Contracts;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using GirlsAgency.MySQL.Data.Migrations;
 
-    public class MySQLContext : DbContext
+    public class MySQLContext : DbContext, IContext
     {
         // Your context has been configured to use a 'MySQLContext' connection string from your application's 
         // configuration file (App.config or Web.config). By default, this connection string targets the 
@@ -16,33 +19,32 @@ namespace GirlsAgency.MySQL.Data
         public MySQLContext()
             : base("name=MySQLContext1")
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MySQLContext, Configuration>());
         }
 
-        public DbSet<BreastSize> BreastSizes { get; set; }
+        public virtual IDbSet<BreastSize> BreastSizes { get; set; }
 
-        public DbSet<City> Cities { get; set; }
+        public virtual IDbSet<City> Cities { get; set; }
 
-        public DbSet<Country> Countries { get; set; }
+        public virtual IDbSet<Country> Countries { get; set; }
 
-        public DbSet<Customer> Customers { get; set; }
+        public virtual IDbSet<Customer> Customers { get; set; }
 
-        public DbSet<Feature> Features { get; set; }
+        public virtual IDbSet<Feature> Features { get; set; }
 
-        public DbSet<Girl> Girls { get; set; }
+        public virtual IDbSet<Girl> Girls { get; set; }
 
-        public DbSet<HairColor> HairColors { get; set; }
+        public virtual IDbSet<HairColor> HairColors { get; set; }
 
-        public DbSet<Order> Orders { get; set; }
+        public virtual IDbSet<Order> Orders { get; set; }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public IDbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
     }
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
